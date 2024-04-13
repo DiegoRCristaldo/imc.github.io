a804db65c13a0595b04f97d0322706e0b1120c07
diegodev <diegorcristaldo@hotmail.com>
2024-04-13 17:55:01 -0300

    Subindo arquivos para o Github

---
+++ index.php
@@ -0,0 +1,32 @@
+<!-- 2 BUGS
+      QUANDO PREENCHE LETRAS AO INVÉS DE NÚMEROS DA ERRO
+      QUANDO COLOCA VIRGULA AO INVES DE PONTO DA ERRO 
+-->
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculo IMC</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik+Scribble&display=swap" rel="stylesheet">
+  </head>
+  <body>
+    <div class="principal">
+      <h1>Calcule seu IMC</h1>
+      <main>
+        <a href="https://api.whatsapp.com/send?phone=5519989909456&text=Gostaria%20de%20modernizar%20minha%20empresa%20com%20um%20site.">
+            <img class="whats" src="assets/imagens/whats.png"/>
+        </a>
+        <form method="get" action="#resultado">
+            <label for="peso">PESO(KG)</label>
+            <input type="text" id="peso" name="peso" placeholder="Qual o seu peso?(kg)" required/>
+            <label for="altura">ALTURA(M)</label>
+            <input type="text" id="altura" name="altura" placeholder="Qual é a sua altura?(m)" required/>
+            <button type="submit">ENVIAR</button>
+        <?php
+          require_once('controlador/calculoimc.php');
+          require_once('visao/rodape.php');
+        ?>
---
+++ visao/rodape.php
@@ -0,0 +1,26 @@
+          <section id="resultado"></section>
+        </form>
+      </main>
+    </div>
+    <footer>
+      <div class="redes">
+        <a href="https://www.facebook.com/profile.php?id=100076215697987">
+          <img class="logo" src="./assets/imagens/facebook.png" alt=""/>
+        </a>
+        <a href="https://www.instagram.com/rcristaldodevweb/">
+          <img class="logo" src="./assets/imagens/instagram.png" alt=""/>
+        </a>
+        <a href="https://api.whatsapp.com/send?phone=5519989909456&text=Gostaria%20de%20modernizar%20minha%20empresa%20com%20um%20site.">
+          <img class="logo" src="./assets/imagens/whats.png" alt=""/>
+        </a>
+        <a href="https://linkedin.com/in/diego-rodrigues-cristaldo-99a53215a">
+          <img class="logo" src="./assets/imagens/linkedin.png" alt=""/>
+        </a>
+      </div>
+      <cite>
+        <a href="https://rcristaldodevweb.netlify.app">&copy;2024 Calcule seu IMC. Todos os direitos reservados. Site desenvolvido por R 
+        Cristaldo Desenvolvimento Web.</a>
+      </cite>    
+    </footer>
+  </body>
+</html>
