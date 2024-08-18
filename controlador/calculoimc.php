9d7868389ce0c0a74d4cc33738da500f55874ca0
diegodev <diegorcristaldo@hotmail.com>
2024-08-17 21:59:05 -0300

    Atualizações de responsividade.

--- controlador/calculoimc.php
+++ controlador/calculoimc.php
@@ -2,46 +2,56 @@
     if(isset($_GET['peso']) && isset($_GET['altura'])){ 
         $peso = $_GET['peso'];
         $altura = $_GET['altura'];
-        
-        function imc($peso, $altura){
-            $imc = $peso / ($altura * $altura);
-            return $imc;
-        }
 
-        if ($peso > 0 && $altura > 0) {
-            $imc = imc($peso, $altura);
-            $faixas = [
-                ["min" => 0, "max" => 18.5, "rank" => "Magreza"],
-                ["min" => 18.51, "max" => 24.99, "rank" => "Saudável"],
-                ["min" => 25, "max" => 29.99, "rank" => "Sobrepeso"],
-                ["min" => 30, "max" => 34.99, "rank" => "Obesidade Grau I"],
-                ["min" => 35, "max" => 39.99, "rank" => "Obesidade Grau II"],
-                ["min" => 40, "max" => PHP_FLOAT_MAX, "rank" => "Obesidade Grau III"]
-            ];
+        $peso = str_replace(',', '.', $peso);
+        $altura = str_replace(',', '.', $altura);
         
-            foreach($faixas as $faixa){
-                $imc = round($imc, 2);
-                if($imc >= $faixa["min"] && $imc <= $faixa["max"]){
-                    if($imc <= 18.5 || $imc >= 30){
-                        echo "<p class='vermelho'>Atenção, seu IMC é $imc, e você está classificado com {$faixa["rank"]}<p>";
-                        require_once('visao/rodape.php');
-                        return;    
-                    }
-                    elseif($imc >= 25 && $imc <= 29.99){
-                        echo "<p class='laranja'>Atenção, seu IMC é $imc, e você está classificado com {$faixa["rank"]}<p>";
-                        require_once('visao/rodape.php');
-                        return;
-                    }
-                    else{
-                        echo "<p>Atenção, seu IMC é $imc, e você está classificado com {$faixa["rank"]}<p>";
-                        require_once('visao/rodape.php');
-                        return;
+        if (!is_numeric($peso) || !is_numeric($altura)) {
+            echo "<p class='vermelho'>Peso e altura devem ser números.</p>";
+            return;
+        }
+        
+        else{
+            function imc($peso, $altura){
+                $imc = $peso / ($altura * $altura);
+                return $imc;
+            }
+    
+            if ($peso > 0 && $altura > 0) {
+                $imc = imc($peso, $altura);
+                $faixas = [
+                    ["min" => 0, "max" => 18.5, "rank" => "Magreza"],
+                    ["min" => 18.51, "max" => 24.99, "rank" => "Saudável"],
+                    ["min" => 25, "max" => 29.99, "rank" => "Sobrepeso"],
+                    ["min" => 30, "max" => 34.99, "rank" => "Obesidade Grau I"],
+                    ["min" => 35, "max" => 39.99, "rank" => "Obesidade Grau II"],
+                    ["min" => 40, "max" => PHP_FLOAT_MAX, "rank" => "Obesidade Grau III"]
+                ];
+            
+                foreach($faixas as $faixa){
+                    $imc = round($imc, 2);            
+                    if($imc >= $faixa["min"] && $imc <= $faixa["max"]){
+                        if($imc <= 18.5 || $imc >= 30){
+                            echo "<p class='vermelho'>Atenção, seu IMC é $imc, e você está classificado com {$faixa["rank"]}<p>";
+                            require_once('visao/rodape.php');
+                            return;    
+                        }
+                        elseif($imc >= 25 && $imc <= 29.99){
+                            echo "<p class='laranja'>Atenção, seu IMC é $imc, e você está classificado com {$faixa["rank"]}<p>";
+                            require_once('visao/rodape.php');
+                            return;
+                        }
+                        else{
+                            echo "<p>Atenção, seu IMC é $imc, e você está classificado com {$faixa["rank"]}<p>";
+                            require_once('visao/rodape.php');
+                            return;
+                        }
                     }
                 }
             }
-        }
-        else{
-            echo "<p class='vermelho'>Peso e altura devem ser números positivos.</p>";
+            else{
+                echo "<p class='vermelho'>Peso e altura devem ser números positivos.</p>";
+            }
         }
     }
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<!-- 2 BUGS
-      QUANDO PREENCHE LETRAS AO INVÉS DE NÚMEROS DA ERRO
-      QUANDO COLOCA VIRGULA AO INVES DE PONTO DA ERRO 
--->
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -14,16 +10,22 @@
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik+Scribble&display=swap" rel="stylesheet">
   </head>
   <body>
-    <div class="principal">
-      <h1>Calcule seu IMC</h1>
-      <main>
-        <form method="get" action="#resultado">
-            <label for="peso">PESO(KG)</label>
-            <input type="text" id="peso" name="peso" placeholder="Qual o seu peso?(kg)" required/>
-            <label for="altura">ALTURA(M)</label>
-            <input type="text" id="altura" name="altura" placeholder="Qual é a sua altura?(m)" required/>
-            <button type="submit">ENVIAR</button>
-        <?php
-          require_once('controlador/calculoimc.php');
-          require_once('visao/rodape.php');
-        ?>
+    <header>
+      <nav class="principal">
+        <h1>
+          <a href="index.php">Calcule seu IMC</a>
+        </h1>
+      </nav>
+    </header>
+    
+    <main>
+      <form method="get" action="#resultado">
+          <label for="peso">PESO(KG)</label>
+          <input type="text" id="peso" name="peso" placeholder="Qual o seu peso?(kg)" required/>
+          <label for="altura">ALTURA(M)</label>
+          <input type="text" id="altura" name="altura" placeholder="Qual é a sua altura?(m)" required/>
+          <button type="submit">ENVIAR</button>
+      <?php
+        require_once('controlador/calculoimc.php');
+        require_once('visao/rodape.php');
+      ?>
